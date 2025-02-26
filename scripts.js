9d032f25207074a4a919eb26b29a42c5704eb93c
Josué Florián <josueflorian12317@gmail.com>
2025-02-25 21:57:53 -0500

    Add project modal functionality and update styles

--- scripts.js
+++ scripts.js
@@ -1,17 +1,24 @@
 function showModal(link) {
-    // Asigna el enlace recibido al atributo src del iframe
     document.getElementById("modalIframe").src = link;
-    // Muestra el modal cambiando su display a "flex"
     document.getElementById("customAlert").style.display = "flex";
-    // Añade el evento para cerrar el modal al pulsar el botón
     document.getElementById("closeModal").addEventListener("click", closeModal);
   }
 
   function closeModal() {
-    // Oculta el modal
+
     document.getElementById("customAlert").style.display = "none";
-    // Limpia el src del iframe para detener el contenido si es necesario
     document.getElementById("modalIframe").src = "";
-    // Remueve el event listener del botón para evitar múltiples asignaciones
     document.getElementById("closeModal").removeEventListener("click", closeModal);
   }
+
+
+  function showProjectModal() {
+    document.getElementById("project").style.display = "flex";
+    document.getElementById("closeprojectModal").addEventListener("click", closeprojectModal);
+  }
+
+
+function closeprojectModal() {
+  document.getElementById("project").style.display = "none";
+  document.getElementById("closeprojectModal").removeEventListener("click", closeModal);
+}
